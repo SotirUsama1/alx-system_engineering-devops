e935ac37a35368d1c20bb404a00576f8e72ecf6e
SotirUsama1 <sotirusama16@gmail.com>
2024-06-30 22:54:07 +0300

    task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,40 +1,22 @@
-# using puppet to make changes to the default ssh config file
-# so that one can connect to a server without typing a password.
+# 100-puppet_ssh_config.pp
 
-include stdlib
-
-file_line { 'SSH Private Key':
-  path               => '/etc/ssh/ssh_config',
-  line               => '    IdentityFile ~/.ssh/school',
-  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
-  replace            => true,
-  append_on_no_match => true
+# Ensure the .ssh directory exists with the correct permissions
+file { '/home/ubuntu/.ssh':
+  ensure  => directory,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0700',
 }
 
-# Regex match explanation
-#
-# ^       beginning of the line
-# [#]*  atleast one hash character
-# [\s]*  zero or more white space characters
-# (?i)IdentityFile case insensitive "IdentityFile"
-# [\s]+ at least one whitespace character
-# ~/.ssh/id_rsa The ssh private key file path we want to replace
-# $      end of the line
-
-file_line { 'Deny Password Auth':
-  path               => '/etc/ssh/ssh_config',
-  line               => '    PasswordAuthentication no',
-  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
-  replace            => true,
-  append_on_no_match => true
+# Ensure the SSH configuration file exists and is correctly configured
+file { '/home/ubuntu/.ssh/config':
+  ensure  => file,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0600',
+  content => @("CONFIG")
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+    | CONFIG
 }
-
-# Regex match explanation
-#
-# ^       beginning of the line
-# [#]*  atleast one hash character
-# [\s]*  zero or more white space characters
-# (?i)PasswordAuthentication case insensitive "PasswordAuthentication"
-# [\s]+ at least one whitespace character
-# (yes|no) with the value "yes" or the value "no"
-# $      end of the line
